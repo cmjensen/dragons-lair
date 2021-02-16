4bbfbbfdc7da2990c2fd5e7d7e72d8ddbd1cda60
Christie Jensen <christiejensen@Christies-MacBook-Air.local>
2021-02-15 22:22:13 -0700

    finished, all tests passed

---
+++ server/controllers/authController.js
@@ -0,0 +1,48 @@
+const bcrypt = require('bcryptjs')
+
+module.exports = { 
+    register: async ( req, res ) => {
+    const { username, password, isAdmin } = req.body
+    //get db instance
+    const db = req.app.get('db')
+    const result = await db.get_user( username )
+    const existingUser = result[0]
+    if( existingUser ){
+        res.status(409).send('Username taken')
+    }
+    let salt = bcrypt.genSaltSync(10)
+    let hash = bcrypt.hashSync( password, salt )
+    const registeredUser = await db.register_user([ isAdmin, username, hash ])
+    const user = registeredUser[0]
+    req.session.user = {
+        isAdmin: user.is_admin,
+        id: user.id,
+        username: user.username
+    }
+    res.status(201).send(req.session.user)
+    },
+    login: async ( req, res ) => {
+        const { username, password } = req.body
+        const db = req.app.get('db')
+        const foundUser = await db.get_user( username )
+        const user = foundUser[0]
+        if( !user ){
+            return res.status(401).send('User not found. Please register as a new user before logging in.')
+        }
+        let isAuthenticated = bcrypt.compareSync( password, user.hash )
+        if( !isAuthenticated ){
+            return res.status(403).send('Incorrect password')
+        } else {
+            req.session.user = {
+                isAdmin: user.is_admin,
+                id: user.id,
+                username: user.username
+            }
+            return res.status(200).send( req.session.user )
+        }
+    },
+    logout: async ( req, res ) => {
+        req.session.destroy()
+        return res.sendStatus(200)
+    }
+}
